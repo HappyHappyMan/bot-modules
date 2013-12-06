cd71067da31543f6e0d54e93ca8f25e527332ba6
Sri Ramanujam <robcharan@yahoo.com>
2013-12-06 00:57:36 -0500

    Re-implemented forecast functionality using forecast.io

--- module_forecast.py
+++ module_forecast.py
@@ -1,39 +1,39 @@
-# -*- coding: utf-8 -*-
+# # -*- coding: utf-8 -*-
 
-"""
-$Id: module_forecast.py 313 2011-06-03 11:22:38Z henri@nerv.fi $
-$HeadURL: http://pyfibot.googlecode.com/svn/trunk/modules/module_forecast.py $
-"""
+# """
+# $Id: module_forecast.py 313 2011-06-03 11:22:38Z henri@nerv.fi $
+# $HeadURL: http://pyfibot.googlecode.com/svn/trunk/modules/module_forecast.py $
+# """
 
-has_pywapi = False
-try:
-    import pywapi
-    has_pywapi = True
-except:
-    print('Error loading library pywapi. Probably you havent installed it yet.')
-
-
-def fahrenheit_to_celcius(f):
-    return (int(f) - 32) / 1.8
+# has_pywapi = False
+# try:
+#     import pywapi
+#     has_pywapi = True
+# except:
+#     print('Error loading library pywapi. Probably you havent installed it yet.')
 
 
-def command_forecast(bot, user, channel, args):
-    """This module tells weather forecast for location"""
-    if not has_pywapi:
-        return
+# def fahrenheit_to_celcius(f):
+#     return (int(f) - 32) / 1.8
 
-    result_dict = pywapi.get_weather_from_google(args)
-    if not all(result_dict.values()):
-        bot.say(channel, 'unknown location')
-        return
 
-    def format_day(day, args):
-        split_args = args.split('/')
-        if split_args[1] == ('c' or 'C'):
-            return (u'%s: %s (%.0f°C/%.0f°C)' % (day['day_of_week'], day['condition'], fahrenheit_to_celcius(day['low']), fahrenheit_to_celcius(day['high'])))
-        if split_args[1] == ('f' or 'F'):
-            return (u'%s: %s (%.0f°F/%.0f°F)' % (day['day_of_week'], day['condition'], float(day['low']), float(day['high'])))
-    answerstr = u'%s: ' % (result_dict['forecast_information']['city'])
-    answerstr += u", ".join(format_day(day, args) for day in result_dict['forecasts'])
+# def command_forecast(bot, user, channel, args):
+#     """This module tells weather forecast for location"""
+#     if not has_pywapi:
+#         return
 
-    bot.say(channel, answerstr.encode('utf-8'))
+#     result_dict = pywapi.get_weather_from_google(args)
+#     if not all(result_dict.values()):
+#         bot.say(channel, 'unknown location')
+#         return
+
+#     def format_day(day, args):
+#         split_args = args.split('/')
+#         if split_args[1] == ('c' or 'C'):
+#             return (u'%s: %s (%.0f°C/%.0f°C)' % (day['day_of_week'], day['condition'], fahrenheit_to_celcius(day['low']), fahrenheit_to_celcius(day['high'])))
+#         if split_args[1] == ('f' or 'F'):
+#             return (u'%s: %s (%.0f°F/%.0f°F)' % (day['day_of_week'], day['condition'], float(day['low']), float(day['high'])))
+#     answerstr = u'%s: ' % (result_dict['forecast_information']['city'])
+#     answerstr += u", ".join(format_day(day, args) for day in result_dict['forecasts'])
+
+#     bot.say(channel, answerstr.encode('utf-8'))
--- module_weather.py
+++ module_weather.py
@@ -7,6 +7,7 @@ import yaml
 import urllib
 import os
 import sqlite3
+import datetime
 
 
 def _import_yaml_data(directory=os.curdir):
@@ -30,7 +31,7 @@ def _get_saved_data(nick, conn):
     c = DB.cursor()
     userdata = ""
 
-    result = c.execute("SELECT temp_type,location FROM weather WHERE nick LIKE ?", (nick,))
+    result = c.execute("SELECT temp_type,location,forecast_type FROM weather WHERE nick LIKE ?", (nick,))
 
     userdata = result.fetchone()
 
@@ -40,7 +41,7 @@ def _get_saved_data(nick, conn):
     except TypeError:
         return userdata, False
 
-def _set_saved_data(nick, location, conn, temp_type):
+def _set_saved_data(nick, location, conn, temp_type, forecast_type):
     """Have them specify a temp_type no matter what in the bot code, it's too much hassle trying to predict it when updating."""
     DB = sqlite3.connect(conn)
     c = DB.cursor()
@@ -54,12 +55,10 @@ def _set_saved_data(nick, location, conn, temp_type):
         worked = False
 
     if worked is True:
-        vartuple = (temp_type, nick, location)
-        ## DEBUG ##
-        print vartuple
-        ## END DEBUG ##
+        vartuple = (temp_type, nick, location, forecast_type)
+
         try:
-            c.execute("UPDATE weather SET temp_type=(?), location=(?) WHERE nick=(?)", (vartuple[0],vartuple[2],vartuple[1]))
+            c.execute("UPDATE weather SET temp_type=(?), location=(?), forecast_type=(?) WHERE nick=(?)", (vartuple[0],vartuple[2],vartuple[3],vartuple[1]))
         except sqlite3.Error as e:
             print e.args[0]
 
@@ -69,12 +68,10 @@ def _set_saved_data(nick, location, conn, temp_type):
 
         return
     else:
-        vartuple = (temp_type, nick, location)
-        ## DEBUG ##
-        print vartuple
-        ## END DEBUG ##
+        vartuple = (temp_type, nick, location, forecast_type)
+
         try:
-            c.execute("INSERT INTO weather VALUES (?, ?, ?)", vartuple)
+            c.execute("INSERT INTO weather VALUES (?, ?, ?, ?)", vartuple)
         except sqlite3.Error as e:
             print e.args[0]
 
@@ -83,6 +80,74 @@ def _set_saved_data(nick, location, conn, temp_type):
         DB.close()
 
         return
+
+def _parse_forecast_output(weather_data, city_name, measure_type):
+
+    temp_suffix = [u"°F", u"°C", u"°C", u"°C"]
+    icons = {'clear-day':u'☀ ', 'clear-night':u'☾ ', 'rain':u'☂ ', 'snow':u'☃ ', 'fog':u'🌁 ', 'cloudy':u'☁ ', 'partly-cloudy-day':u'⛅ '}
+
+    forecast_list = []
+
+    for x in range(3):
+        try:
+            icon = icons[weather_data['daily'][x]['icon']]
+        except KeyError:
+            icon = ""
+        date = datetime.datetime.fromtimestamp(int(weather_data['daily']['data'][x]['time'])).strftime("%Y-%m-%d") # to be changed
+        summary = weather_data['daily']['data'][x]['summary']
+        if measure_type == 4:
+            minTemp_f = str(weather_data['daily']['data'][x]['temperatureMin'])
+            maxTemp_f = str(weather_data['daily']['data'][x]['temperatureMax'])
+            minTemp_c = str(round((weather_data['daily']['data'][x]['temperatureMin'] - 32) * 5/9, 2))
+            maxTemp_c = str(round((weather_data['daily']['data'][x]['temperatureMax'] - 32) * 5/9, 2))
+
+            buildstr =  "%s: %s%s High: %s%s (%s%s) Low: %s%s (%s%s)" % (date, icon, summary, minTemp_f, u"°F", minTemp_c, u"°C", maxTemp_f, u"°F", maxTemp_c, u"°C")         
+            forecast_list.append(buildstr)
+
+        else:
+            minTemp = str(weather_data['daily']['data'][x]['temperatureMin'])
+            maxTemp = str(weather_data['daily']['data'][x]['temperatureMax'])
+
+            buildstr = date + ": " + icon + summary + " High: " + maxTemp + temp_suffix[measure_type] + " Low: " + minTemp + temp_suffix[measure_type]
+            forecast_list.append(buildstr)
+
+    return forecast_list
+
+def _parse_args(args):
+    """Parses an already-split args list and returns a list with location, unit type, and forecast type, in that order."""
+    if len(args) > 1: # If there are any optional args at all
+        if args[1] in ["us", "si", "uk", "ca", "both"]: # If the first arg is for units, set up units
+            if args[1] == "us":
+                args[1] = 0
+            elif args[1] == "si":
+                args[1] = 1
+            elif args[1] == "ca":
+                args[1] = 2
+            elif args[1] == "uk":
+                args[1] = 3
+            elif args[1] == "both":
+                args[1] = 4
+
+            try:  
+                args[2] # If there is a second arg
+                if not args[2] in ["brief", "summary"]:
+                    args[2] = "summary"
+            except IndexError: # If only units were specified, set default forecast arg
+                args.append("summary")
+        elif args[1] in ["brief", "summary"]: # If the first arg isn't for units, set the forecast arg and default units
+            args.append(args[1])
+            args[1] = 0
+    else: # If no options were specified, set default units and forecast
+        args.append(0)
+        args.append("summary")
+
+    ## DEBUG ##
+    print args[0]
+    print args[1]
+    print args[2]
+    ## END DEBUG ##
+
+    return args
 
 def _parse_weather_output(weather_data, city_name, measure_type):
 
@@ -113,7 +178,7 @@ def _parse_weather_output(weather_data, city_name, measure_type):
     return weather_string
 
 def command_weather(bot, user, channel, args):
-    """Gives weather for location. Enter a city or zip code"""
+    """Gives weather for location. Follows the same syntax as .wadd."""
 
     settings = _import_yaml_data()
     measure_type = 0
@@ -124,20 +189,20 @@ def command_weather(bot, user, channel, args):
         args = args.split(":")
         latlng = _get_latlng(args[0])
         units = ""
+        args = _parse_args(args)
 
-        if len(args) > 1:
-            if args[1] == "si":
-                units = "?units=si"
-                measure_type = 1
-            elif args[1] == "ca":
-                units = "?units=ca"
-                measure_type = 2
-            elif args[1] == "uk":
-                units = "?units=uk"
-                measure_type = 3
-            elif args[1] == "both":
-                units = "?units=us"
-                measure_type = 4
+        if args[1] == "si":
+            units = "?units=si"
+            measure_type = 1
+        elif args[1] == "ca":
+            units = "?units=ca"
+            measure_type = 2
+        elif args[1] == "uk":
+            units = "?units=uk"
+            measure_type = 3
+        elif args[1] == "both":
+            units = "?units=us"
+            measure_type = 4
 
         print latlng
         print type(latlng)
@@ -161,7 +226,6 @@ def command_weather(bot, user, channel, args):
                 units = "?units=uk"
             elif measure_type == 4:
                 units = "?units=us"
-                bot.say(nick, "I hate you for making me do this.")
             data = urllib2.urlopen(API_URL % (settings["weather"]["key"], latlng[0]) + units)
         else:
             bot.say(channel, "You're not in the database! Set a location with .wadd.")
@@ -178,36 +242,84 @@ def command_weather(bot, user, channel, args):
 
     return
 
+def command_forecast(bot, user, channel, args):
+    """Gives you a forecast. To use, add either :brief or :summary to your weather query. :brief will PM you always."""
+    API_URL = "https://api.forecast.io/forecast/%s/%s"
+    settings = _import_yaml_data()
+    nick = user.split('!', 1)[0]
+
+    if args:
+        args = args.split(":")
+        args = _parse_args(args)
+        latlng = _get_latlng(args[0])
+        units = ""
+
+        measure_type = args[1]
+
+        if measure_type == 1:
+            units = "?units=si"
+        elif measure_type == 2:
+            units = "?units=ca"
+        elif measure_type == 3:
+            units = "?units=uk"
+        elif measure_type == 4:
+            units = "?units=us"
+    else:
+        db_data, success = _get_saved_data(nick.lower(), settings['weather']['db'])
+        if success is True:
+            units = ""
+            latlng = _get_latlng(db_data[1])
+            measure_type = db_data[0]
+            if measure_type == 1:
+                units = "?units=si"
+            elif measure_type == 2:
+                units = "?units=ca"
+            elif measure_type == 3:
+                units = "?units=uk"
+            elif measure_type == 4:
+                units = "?units=us"
+        else:
+            bot.say(channel, "You're not in the database! Set a location with .wadd.")
+            return
+
+    data = urllib2.urlopen(API_URL % (settings["weather"]["key"], latlng[0]) + units)
+
+    weather_data = json.load(data)
+
+    city_name = latlng[1].encode('utf-8')
+
+    try:
+        ftype = args[2]
+    except IndexError:
+        ftype = db_data[2]
+
+    if ftype == "summary":
+        bot.say(channel, "Forecast summary for \x02%s\x02 \x02\x033|\x03\x02 %s" % (city_name, weather_data['daily']['summary'].encode('utf-8')))
+    else:
+        forecast_strings = _parse_forecast_output(weather_data, city_name, measure_type)
+        bot.say(nick, "Three-day forecast for \x02%s\x02" % city_name)
+        for line in forecast_strings:
+            bot.say(nick, line.encode('utf-8'))
+
 def command_wadd(bot, user, channel, args):
-    """Sets your location. Usage: .wadd location:units, where units can be any one of us, si, ca, uk. Defaults to us if no unit is specified."""
+    """Sets your location. Usage: .wadd location:units:forecast, Units can be any one of us, si, ca, uk. Forecast is either brief or summary. Defaults to us/summary if no unit/forecast is specified."""
 
     settings = _import_yaml_data()
 
     args = args.split(":")
     print args
     nick = user.split('!', 1)[0]
-    try:
-        if args[1] == "us":
-            args[1] = 0
-        elif args[1] == "si":
-            args[1] = 1
-        elif args[1] == "ca":
-            args[1] = 2
-        elif args[1] == "uk":
-            args[1] = 3
-        elif args[1] == "both":
-            args[1] = 4
-    except IndexError:
-        args.append(0)
 
-    print nick
-    print args[0]
-    print settings["weather"]["key"]
-    print args[1]
+    args = _parse_args(args)
 
-    _set_saved_data(nick.lower(), args[0], settings['weather']['db'], args[1])
+    _set_saved_data(nick.lower(), args[0], settings['weather']['db'], args[1], args[2])
 
     bot.say(nick, "Location set!")
 
 def command_w(bot, user, channel, args):
+    """Gives weather for location. Follows the same syntax as .wadd."""
     return command_weather(bot, user, channel, args)
+
+def command_f(bot, user, channel, args):
+    """Alias to forecast. Uses the same syntax."""
+    return command_forecast(bot, user, channel, args)
