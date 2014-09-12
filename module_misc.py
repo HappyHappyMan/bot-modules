b52976c4c77179aae62de8ab7c086d5340e5db80
Sri Ramanujam <robcharan@yahoo.com>
2014-09-12 14:15:54 -0400

    Added 8ball command

--- module_misc.py
+++ module_misc.py
@@ -109,3 +109,30 @@ def command_doge(bot, user, channel, args):
         return
     bot.say(channel, "1 dogecoin is worth %s USD." % amount)
     return
+
+def command_8ball(bot, user, channel, args):
+    """It's an 8ball function, it does exactly what you think it does."""
+    from random import randint
+    response_list = ["It is certain",
+            "It is decidedly so",
+            "Without a doubt",
+            "Yes definitely",
+            "You may rely on it",
+            "As I see it, yes",
+            "Most likely",
+            "Outlook good",
+            "Yes",
+            "Signs point to yes",
+            "Reply hazy try again",
+            "Ask again later",
+            "Better not tell you now",
+            "Cannot predict now",
+            "Concentrate and ask again",
+            "Don't count on it",
+            "My reply is no",
+            "My sources say no",
+            "Outlook not so good",
+            "Very doubtful"
+            ]
+    bot.say(channel, "%s: %s" % (bot.factory.getNick(user), response_list[randint(0, len(response_list) - 1)]))
+    return
